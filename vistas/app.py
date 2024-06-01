5e576b0c56045c12fe988f7336d69d46b6fcb7aa
cristopher-loyola <157190820+cristopher-loyola@users.noreply.github.com>
2024-05-31 20:24:14 -0600

    hasta aqui se ha llehgado en la clase del 31 de mayo

---
+++ vistas/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request,render_template
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='bdflask'
+
+mysql= MySQL(app)
+
+#Manejo de excepciones
+@app.errorhandler(404)
+def paginano (e):
+    return 'Revisar tu sintaxis: No encontre nada'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/GuardarAlbum', methods=['POST'])
+def guardarAlbum():
+    if request.method == 'POST':
+        titulo=request.form['txtTitulo']
+        artista=request.form['txtArtista']
+        año=request.form['txtAño']
+        print(titulo,artista,año)
+        return 'Datos recibidos en el server'
+
+if __name__ == '__main__':
+    app.run(port=2000, debug= True)
