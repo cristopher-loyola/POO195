b4870128911f7258795149f505f983e6f0c4e876
cristopher-loyola <157190820+cristopher-loyola@users.noreply.github.com>
2024-06-12 22:09:28 -0600

    practica 6 terminada funcionando

--- pruebaFlask/app.py
+++ pruebaFlask/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_mysqldb import MySQL
 
-
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -10,17 +9,14 @@ app.config['MYSQL_DB'] = 'tbmedicos'
 
 app.secret_key = 'mysecretkey'
 
-
 mysql = MySQL(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/registro')
+@app.route('/registro', methods=['GET', 'POST'])
 def formulario():
-    return render_template('GuardarAlbum.html')
-def guardarMedico():
     if request.method == 'POST':
         try:
             # Tomamos los datos que vienen por POST
@@ -33,14 +29,14 @@ def guardarMedico():
             
             # Enviamos a la BD
             cursor = mysql.connection.cursor()
-            cursor.execute('INSERT INTO medicos(nombre, rfc, cedula_profesional, correo, contraseña, rol) VALUES(%s, %s, %s, %s, %s, %s)', (Fnombre, Frfc, FcedulaP, FcorreoE, Fcontraseña, Frol))
+            cursor.execute('INSERT INTO tbmedicos (nombre, rfc, cedulaP, correoE, contraseña, rol) VALUES (%s, %s, %s, %s, %s, %s)', (Fnombre, Frfc, Fcedula, Fcorreo, Fcontraseña, Frol))
             mysql.connection.commit()
             flash('Médico registrado correctamente')
-            return redirect(url_for('index'))
+            return redirect(url_for('home'))
         except Exception as e:
             flash('Error al registrar el médico: ' + str(e))
-            return redirect(url_for('index'))
-
+            return redirect(url_for('formulario'))
+    return render_template('GuardarAlbum.html')
 
 if __name__ == '__main__':
     app.run(port=2000, debug=True)
